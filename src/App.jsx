50459c36470b0f95a19b2c1c9342bb8a85f5b4cb
Shermaine <shermaine.lsh@gmail.com>
2024-07-03 12:06:54 +0800

    edit: work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import panda from "./assets/Panda.png";
+import babyPanda from "./assets/Baby Panda.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <h1>WORK IN PROGRESS</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Hi! I'm Shermaine and this is my portfolio site. Pardon me but it is
+          still under development.
         </p>
+        <p>⬇️⬇️⬇️ Meanwhile, visit my GitHub! ⬇️⬇️⬇️</p>
+        <a href="https://github.com/koyostar" target="_blank">
+          <img src={panda} className="panda" alt="Go to my github" />
+        </a>
+        <a href="https://github.com/koyostar/thePandaGame" target="_blank">
+          <img src={babyPanda} className="babypanda" alt="my first game" />
+        </a>
+        <a href="https://github.com/koyostar/travel-buddy" target="_blank">
+          <img src={babyPanda} className="babypanda" alt="travel planner app" />
+        </a>
+        <a href="https://github.com/koyostar/DDNoMore_FE" target="_blank">
+          <img
+            src={babyPanda}
+            className="babypanda"
+            alt="task management app"
+          />
+        </a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="card">
+        <p>OR</p>
+        <a href="mailto:shermaine.lsh@gmail.com?subject=Hi%20Shermaine&body=Hi%20Shermaine,%0D%0A%0D%0A">
+          <button>Send me an email</button>
+        </a>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
