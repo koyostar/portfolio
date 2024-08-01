3c81a1aab1668ff4a84e6c4ade9fec8fced91af2
Shermaine <shermaine.lsh@gmail.com>
2024-08-02 00:47:26 +0800

    feature: completed Videos and Outreach

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from "./components/ContactForm";
 import Portfolio from "./pages/Portfolio";
 import Navbar from "./components/Navbar";
 import FloatingButton from "./components/FloatingButton";
@@ -16,7 +15,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/portfolio/*" element={<Portfolio />} />
         </Routes>
       </div>
     </Router>
